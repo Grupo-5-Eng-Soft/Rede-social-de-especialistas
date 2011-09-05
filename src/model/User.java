832938e00b6f2c54e25716bde5fd96adebe40a0a
Felipe Torres <felipe.gtorres@gmail.com>
2011-09-05 16:41:46 -0300

    Campos do form de cadastro de usuarios nao podem ser vazios.

--- src/model/User.java
+++ src/model/User.java
@@ -4,14 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.hibernate.validator.NotEmpty;
+
 @Entity
 public class User {
 	@Id
 	@GeneratedValue
 	private int id;
 	
+	@NotEmpty
 	private String login;
+	
+	@NotEmpty
 	private String password;
+	
+	@NotEmpty
 	private String email;
 
 	public String getLogin() {
