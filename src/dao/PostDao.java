53d2a6ed0fb3f26c3cbea651499ab2abbe545d03
Felipe Torres <felipe.gtorres@gmail.com>
2011-09-15 19:37:26 -0300

    Tirando o erro ao acessar /post/new. Só coloquei o construtor no PostDao.

--- src/dao/PostDao.java
+++ src/dao/PostDao.java
@@ -2,7 +2,18 @@ package dao;
 
 import model.Post;
 
+import org.hibernate.Session;
+
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
 public class PostDao {
+	
+	private final Session session;
+
+	public PostDao(Session session) {
+		this.session = session;
+	}
 
 	public void save(Post post) {
 		
