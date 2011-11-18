361c1d38cdee3d9f995ce15e6d580006134e5eed
Francisco Sokol <chico.sokol@gmail.com>
2011-11-18 09:44:53 -0200

    novos testes para EmailConfirmationController

--- test/controller/EmailConfirmationControllerTest.java
+++ test/controller/EmailConfirmationControllerTest.java
@@ -1,6 +1,8 @@
 package controller;
 
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import hash.HashCalculator;
+import model.EmailConfirmation;
 import model.User;
 
 import org.junit.Before;
@@ -31,6 +33,51 @@ public class EmailConfirmationControllerTest {
 		User user = validUser();
 		controller.createAndSendEmailConfirmation(user, null, false);
 		verify(dao).saveEmailConfirmationFromUser(user, false);
+	}
+	
+	@Test
+	public void shouldConfirmUser() {
+		User user = validUser();
+		user.setActive(false);
+		HashCalculator hash = new HashCalculator(user.getLogin() + user.getEmail());
+		String confirmationString = hash.getValue();
+		EmailConfirmation confirmation = new EmailConfirmation();
+		confirmation.setUser(user);
+		when(users.getUser(user.getId())).thenReturn(user);
+		when(dao.getEmailConfirmation(user)).thenReturn(confirmation);
+		controller.confirmUser(user.getId(), confirmationString);
+		verify(dao).getEmailConfirmation(user);
+	}
+	
+	@Test
+	public void shouldNotConfirmUserWithInvalidID() {
+		when(users.getUser(666)).thenReturn(null);
+		controller.confirmUser(666, "hashqualquer");
+		verify(users).getUser(666);
+	}
+	
+	@Test
+	public void shouldNotConfirmUserWithActiveUser() {
+		User user = validUser();
+		user.setActive(true);
+		String confirmationString = "hashqualquer";
+		when(users.getUser(user.getId())).thenReturn(user);
+		controller.confirmUser(user.getId(), confirmationString);
+		verify(dao, never()).removeEmailConfirmationFrom(user);
+	}
+	
+	@Test
+	public void shouldNotConfirmUserWithWrongHash() {
+		User user = validUser();
+		user.setActive(false);
+		HashCalculator hash = new HashCalculator(user.getLogin() + user.getEmail());
+		String confirmationString = hash.getValue();
+		EmailConfirmation confirmation = new EmailConfirmation();
+		confirmation.setUser(user);
+		when(users.getUser(user.getId())).thenReturn(user);
+		when(dao.getEmailConfirmation(user)).thenReturn(confirmation);
+		controller.confirmUser(user.getId(), "HASH ERRADA");
+		verify(dao, never()).removeEmailConfirmationFrom(user);
 	}
 
 	private User validUser() {
