f1f6c2102cbd1bc7cf90bf4d0f300dd071c9bd36
Felipe Torres <felipe.gtorres@gmail.com>
2011-11-21 20:21:26 -0200

    Testes para UserDao

---
+++ test/dao/UserDaoTest.java
@@ -0,0 +1,62 @@
+package dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import model.Specialty;
+import model.User;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class UserDaoTest {
+	
+	private UserDao dao;
+	private @Mock Session session;
+	private @Mock Transaction tx;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		when(session.beginTransaction()).thenReturn(tx);
+		dao = new UserDao(session);
+	}
+	
+	@Test
+	public void shouldSaveUserAndCommitTransaction() throws Exception {
+		User user = new User();
+		dao.save(user);
+		verify(session).save(user);
+		verify(tx).commit();
+	}
+	
+	@Test
+	public void shouldSaveUserWithSpecialty() throws Exception {
+		User user = new User();
+		ArrayList<Long> specialties = mockSpecialtyWithId(1l);
+		
+		dao.save(user, specialties);
+		
+		verify(session).save(user);
+		verify(session).update(user);
+		verify(tx).commit();
+		
+		assertEquals(1, user.getSpecialists().size());
+	}
+	
+	private ArrayList<Long> mockSpecialtyWithId(long id) {
+		ArrayList<Long> specialties = new ArrayList<Long>();
+		specialties.add(id);
+		Specialty specialty = mock(Specialty.class);
+		when(session.get(Specialty.class, id)).thenReturn(specialty);
+		return specialties;
+	}
+}
