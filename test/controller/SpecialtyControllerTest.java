0d38fa8c0fe62025986f192dae1582f837ae7854
Francisco Sokol <chico.sokol@gmail.com>
2011-11-30 10:30:31 -0200

    novos testes para o SpecialtyController

--- test/controller/SpecialtyControllerTest.java
+++ test/controller/SpecialtyControllerTest.java
@@ -20,7 +20,7 @@ import dao.SpecialtyDao;
 public class SpecialtyControllerTest {
 	
 	private @Mock SpecialtyDao dao;
-	private Result result = new MockResult();
+	private MockResult result = new MockResult();
 	private SpecialtyController controller;
 	private UserSession session = new UserSession();
 	private Validator validator = new MockValidator();
@@ -33,20 +33,59 @@ public class SpecialtyControllerTest {
 
 	@Test
 	public void shouldSaveSpecialty() throws Exception {
-		Specialty s = new Specialty();
-		s.setName("vraptor");
+		Specialty s = validSpecialty();
 		controller.save(s);
 		verify(dao).save(s);
 	}
 	
 	@Test(expected=ValidationException.class)
 	public void shouldNotSaveSpecialtyWithSameName() throws Exception {
-		Specialty vraptor = new Specialty();
-		vraptor.setName("vraptor");
-		
+		Specialty vraptor = validSpecialty();
 		when(dao.getSpecialtyByName("vraptor")).thenReturn(vraptor);
 		
 		controller.save(vraptor);
+	}
+
+	private Specialty validSpecialty() {
+		Specialty vraptor = new Specialty();
+		vraptor.setName("vraptor");
+		vraptor.setId(1L);
+		return vraptor;
+	}
+	
+	@Test
+	public void shouldDetailSpecialty() {
+		Specialty spec = validSpecialty();
+		when(dao.getSpecialty(spec.getId())).thenReturn(spec);
+		controller.detail(spec.getId());
+		result.included("specialty");
+	}
+	
+	@Test
+	public void shouldUpdateSpecialty() {
+		Specialty updatedSpec = validSpecialty();
+		controller.update(updatedSpec.getId(), updatedSpec);
+		verify(dao).saveOrUpdate(updatedSpec);
+	}
+	
+	@Test
+	public void shouldDeleteSpecialty() {
+		Specialty spec = validSpecialty();
+		controller.delete(spec.getId());
+		verify(dao).delete(spec.getId());
+	}
+	
+	@Test
+	public void shouldEditSpecialty() {
+		Specialty spec = validSpecialty();
+		controller.edit(spec.getId());
+		result.included("specialty");
+	}
+	
+	@Test
+	public void shouldListSpecialties() {
+		controller.list();
+		result.included("specialties");
 	}
 
 }
